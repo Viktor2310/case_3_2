662a9802685d8f6bd5457fada4337d01e3433cec
Viktor2310 <later_84@mail.ru>
2022-03-11 12:20:54 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  if (response.statusCode == 200) {
+
+       return Album.fromJson(jsonDecode(response.body));
+  } else {
+
+    throw Exception('Failed to load album');
+  }
+}
+
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId : json['userId'],
+      id : json['id'],
+      title : json['title'],
+      body : json['body'],
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Вывод данных  JSON',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Вывод данных из JSON'),
+        ),
+        body: Container(
+          //padding: const EdgeInsets.symmetric(horizontal: 36),
+          child: Center(
+            child: FutureBuilder<Album>(
+              future: futureAlbum,builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text( 'Title:  ' + snapshot.data!.title +  '\n' +
+                        'Body:  '+  snapshot.data!.body,
+                    style :  const TextStyle(fontSize : 20,  color : Color(0xFF5d60e6)),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
